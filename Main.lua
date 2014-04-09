d9846d7485234330ff8313a36798dd154dde6ae0
Cory LaFlamme <me@corylaflamme.com>
2014-04-09 00:08:43 -0400

    Fixed the visibility check to not depend on the compass anymore.

--- Main.lua
+++ Main.lua
@@ -7,7 +7,15 @@ WM = WINDOW_MANAGER
 xp = { sv = {}, events = {} }
 ui = {}
 
-xp.config = { name = 'SimpleXP', svName = 'SimpleXP_SavedVariables' }
+xp.config = {
+	name = 'SimpleXP',
+	svName = 'SimpleXP_SavedVariables',
+	framesToCheck = {
+		ZO_MainMenuCategoryBar,
+		ZO_InteractWindow,
+		ZO_GameMenu_InGame
+	}
+}
 
 function xp.createControls()
 
@@ -73,10 +81,22 @@ end
 function xp.onUpdate()
 
 	-- Just uses any old control from the menu to see if it's visible
-	if ZO_Compass:IsHidden() and (sxpHeaderExperienceBar:IsHidden()) then
+	if sxpHeaderExperienceBar:IsHidden() and xp.isHidden() then
 		SimpleXP:SetAlpha(0)
 	else
 		SimpleXP:SetAlpha(1)
 	end
+
+end
+
+function xp.isHidden()
+
+	for _, frame in pairs(xp.config.framesToCheck) do
+		if not frame:IsHidden() then
+			return true
+		end
+	end
+
+	return false
 
 end
